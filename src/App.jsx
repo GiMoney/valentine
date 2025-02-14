34240ca6eef4b68442e4f140d97668e13e30507f
Gi Suen <gsuen6@gatech.edu>
2025-02-13 22:42:05 -0500

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -49,11 +49,11 @@ export default function Page() {
       ) : (
         <>
           <img
-            src={lovesvg}
+            src={}
             className="fixed animate-pulse top-10 md:left-24 left-6 md:w-40 w-28"
           />
           <img
-            src={lovesvg2}
+            src={}
             className="fixed bottom-16 -z-10 animate-pulse md:right-24 right-10 md:w-40 w-32"
           />
           <img
